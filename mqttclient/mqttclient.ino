c4de2c15082c855c83f57bd15676e794f041f9de
Horvath IM <horvath.istvan.mate@gmail.com>
2018-05-12 16:25:45 +0200

    Mqtt pub client

---
+++ mqttclient/mqttclient.ino
@@ -0,0 +1,187 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN            2         // Pin which is connected to the DHT sensor.
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE           DHT11     // DHT 11 
+//#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
+
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+
+#include <ESP8266WiFi.h>
+#include <MQTT.h>
+
+void myDataCb(String& topic, String& data);
+void myPublishedCb();
+void myDisconnectedCb();
+void myConnectedCb();
+
+#define CLIENT_ID "client1"
+
+// create MQTT object
+MQTT myMqtt(CLIENT_ID, "192.168.2.200", 1883);
+
+//
+const char* ssid     = "ESPap";
+const char* password = "thereisnospoon";
+
+bool mqttConnected=false;
+
+//
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  // ============================================================
+  // Initialize device.
+  dht.begin();
+  Serial.println("DHTxx Unified Sensor Example");
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Temperature");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
+  Serial.println("------------------------------------");
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Humidity");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
+  Serial.println("------------------------------------");
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+  // ============================================================
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Connecting to MQTT server");  
+
+  // setup callbacks
+  myMqtt.onConnected(myConnectedCb);
+  myMqtt.onDisconnected(myDisconnectedCb);
+  myMqtt.onPublished(myPublishedCb);
+  myMqtt.onData(myDataCb);
+  
+  Serial.println("connect mqtt...");
+  myMqtt.connect();
+  while(mqttConnected==false){
+    delay(200);
+    Serial.print(".");
+    }
+  Serial.println("subscribe to topic...");
+  myMqtt.subscribe("/IoT-hw/#");
+  delay(10);
+}
+
+//
+void loop() {
+
+  int value = 0;
+
+  // ========================================================================================
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else {
+    Serial.print("Temperature: ");
+    Serial.print(event.temperature);
+    Serial.println(" *C");
+    value = event.temperature;
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+  else {
+    Serial.print("Humidity: ");
+    Serial.print(event.relative_humidity);
+    Serial.println("%");
+  }
+  // ========================================================================================
+
+
+
+  String topic("/IoT-hw");
+  topic += "/temperature";
+  
+  String valueStr(value);
+  Serial.println(valueStr);
+  Serial.println(topic);
+
+  // publish value to topic
+  boolean result = myMqtt.publish(topic, valueStr);
+  Serial.println("   *  message sent");
+    
+  delay(1000);
+}
+
+
+/*
+ * 
+ */
+void myConnectedCb()
+{
+  Serial.println("connected to MQTT server");
+  mqttConnected=true;
+}
+
+void myDisconnectedCb()
+{
+  Serial.println("disconnected. try to reconnect...");
+  delay(500);
+  myMqtt.connect();
+}
+
+void myPublishedCb()
+{
+  Serial.println("published.");
+}
+
+void myDataCb(String& topic, String& data)
+{
+  
+  Serial.print(topic);
+  Serial.print(": ");
+  Serial.println(data);
+}
